6f5fd91f29a099a9255ac96bc0853542cd28cf7a
Tamar <asashvili@gmail.com>
2020-03-20 16:47:50 +0100

    signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { Form, Button, Icon, Message, Segment } from 'semantic-ui-react';
+import Link from 'next/link';
+import catchErrors from '../utils/catchErrors';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
+import { handleLogin } from '../utils/auth'
+
+
 function Signup() {
-  return <>signup</>;
+  return <>
+    <Message
+      attached
+      icon='settings'
+      header='Get started!'
+      content='Create a new account'
+      color='pink'
+    />
+  </>;
 }
 
 export default Signup;
