dcb7e1e1ee29e629adc4885a69c118bdd7488aea
Tamar <asashvili@gmail.com>
2020-04-02 20:47:38 +0200

    create cart permittion to  admin

--- components/Account/AccountPermissions.js
+++ components/Account/AccountPermissions.js
@@ -52,9 +52,28 @@ function AccountPermissions() {
 function UserPermssion({ user }) {
   const [admin, setAdmin] = React.useState(user.role === 'admin')
 
+  const isFirstRun = React.useRef(true)
+
+  React.useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+    updatePermission()
+  }, [admin])
+
   function handleChangePermission() {
     setAdmin(prevState => !prevState)
   }
+
+  async function updatePermission() {
+    const url = `${baseUrl}/api/account`
+    const payload = { _id: user._id, role: admin ? 'admin' : 'user' }
+    await axios.put(url, payload)
+
+  }
+
+
 
   return (
     <Table.Row>
